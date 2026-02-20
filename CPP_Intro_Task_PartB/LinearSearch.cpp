762deec9ae45d19723df9f968b1c86a706c01de8
Te Poro <teporofletcher47@gmail.com>
2026-02-20 13:43:59 +1100

    wrote out a simple Linear Search function

--- CPP_Intro_Task_PartB/LinearSearch.cpp
+++ CPP_Intro_Task_PartB/LinearSearch.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+int LinearSearch(int array, int target);
+
 int main()
 {
 
+}
+
+int LinearSearch(int array[], int target)
+{
+	int size = sizeof(array) / sizeof(array[0]);
+	
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] == target)
+		{
+			return i;
+		}
+	}
+	return -1;
 }
