290563945b6e5ffe609495c79e1344cc47d957b5
Te Poro <teporofletcher47@gmail.com>
2026-02-20 13:53:14 +1100

    Staged some changes

--- CPP_Intro_Task_PartB/LinearSearch.cpp
+++ CPP_Intro_Task_PartB/LinearSearch.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-int LinearSearch(int array, int target);
+int LinearSearch(int array[], int size, int target);
+
+int array00[] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
 
 int main()
 {
-
+	cout << "Target is at index: " << LinearSearch(array00, 20, 3);
 }
 
-int LinearSearch(int array[], int target)
+int LinearSearch(int array[], int size, int target)
 {
-	int size = sizeof(array) / sizeof(array[0]);
-	
 	for (int i = 0; i < size; i++)
 	{
 		if (array[i] == target)
