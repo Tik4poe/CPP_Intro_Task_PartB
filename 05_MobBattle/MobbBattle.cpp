a85c0a2ed79ecdb9ea4cc2db140a1306b0b65f04
Te Poro <teporofletcher47@gmail.com>
2026-02-20 22:39:16 +1100

    setup Mob battle ready for testing and then bug fixing

---
+++ 05_MobBattle/MobbBattle.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+struct mob
+{
+	string name = "";
+	int health = 0;
+	int damage = 0;
+
+	int Attack(mob target)
+	{
+		target.health -= damage;
+		cout << "The " << name << " hit the " << target.name << " for " << damage << " damage." << endl;
+	}
+};
+
+void Battle(mob a, mob b);
+
+int rng = rand() % 2;
+
+int main()
+{
+
+}
+
+void Battle(mob a, mob b)
+{
+	if (rng == 1)
+	{
+		a.Attack(b);
+	}
+	else
+	{
+		b.Attack(a);
+	}
+}
