3e06afb3cb1f21c4b55d8eae98b32855f3aa2743
Te Poro <teporofletcher47@gmail.com>
2026-02-20 15:16:24 +1100

    fixed layout and add comments

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -14,16 +14,32 @@ int main()
 
 void BubbleSort(int array[], int size)
 {
-	for (int i = 0; i < size -1; i++)
+	pair<int, int> p;// create pair once
+
+	// outer loop for each element in the array
+	for (int i = 0; i < size - 1; i++)
 	{
+		// at each element compare and sort
 		for (int j = 0; j < size - i - 1; j++)
 		{
-			int temp = array[j];
-			if (array[j] > array[j + 1])
+			int temp = array[j];// assign current to temp
+			if (array[j] > array[j + 1])// compare current to adjacent(left)
 			{
+				// if current larger than swap
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 			}
+		}
+	}
+
+	// once sorted then check if sort is correct
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			p.first = array[j];// assign current to first
+			p.second = array[j + 1];// assign adjacent(left) to second
+			assert(p.first <= p.second);// compare
 		}
 	}
 }
