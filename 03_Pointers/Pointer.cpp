e30430fc6abab651eed2bf057ba6cfd8366efef0
Te Poro <teporofletcher47@gmail.com>
2026-02-20 22:17:52 +1100

    completed Pointer Task build and runs no errors

--- 03_Pointers/Pointer.cpp
+++ 03_Pointers/Pointer.cpp
@@ -7,7 +7,13 @@ void PointerSwap(int* a, int* b);
 
 int main()
 {
+	int a = 8;
+	int b = 69;
 
+	PointerSwap(&a, &b);
+
+	assert(a == 69);
+	assert(b == 8);
 }
 
 // use temp var to help swap values
