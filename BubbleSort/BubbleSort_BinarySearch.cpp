8e745e724134da130780eca3d800b78d8e40307f
Te Poro <teporofletcher47@gmail.com>
2026-02-20 16:18:02 +1100

    removed the BinarySearch project and rearranged Solution to have BinarySearch in the bubbleSort project and have written the BinarySearch Function

--- BubbleSort/BubbleSort_BinarySearch.cpp
+++ BubbleSort/BubbleSort_BinarySearch.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <vector>
 #include <cassert>
 
 using namespace std;
 
-void BubbleSort(int array[], int size);
+void BubbleSort(vector<int> array);
+int BinarySearch(vector<int> array, int key);
 
-int array01[] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
+vector<int> array01 = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
 
 int main()
 {
-	BubbleSort(array01, 20);
+	BubbleSort(array01);
+
+	BinarySearch(array01, 83);
+
+	cout << "|========== END ===========|" << endl;
 }
 
-void BubbleSort(int array[], int size)
+void BubbleSort(vector<int> array)
 {
 	pair<int, int> p;// create pair once
+
+	int size = array.size();
 
 	// outer loop for each element in the array
 	for (int i = 0; i < size - 1; i++)
@@ -42,4 +50,30 @@ void BubbleSort(int array[], int size)
 			assert(p.first <= p.second);// compare
 		}
 	}
+}
+
+int BinarySearch(vector<int> array, int target)
+{
+	int low = 0;
+	int high = array.size() - 1;
+
+	while (low <= high)
+	{
+		int mid = low + (high - low) / 2;
+		
+		if (array[mid] == target)
+		{
+			return mid;
+		}
+
+		if (array[mid] < target)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+	return -1;
 }
