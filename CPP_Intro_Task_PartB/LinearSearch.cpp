802c45ac0e1de8dd1e102888757130a47882d535
Te Poro <teporofletcher47@gmail.com>
2026-02-20 14:02:32 +1100

    Completed LinearSeach Task

--- CPP_Intro_Task_PartB/LinearSearch.cpp
+++ CPP_Intro_Task_PartB/LinearSearch.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -8,7 +9,17 @@ int array00[] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
 
 int main()
 {
-	cout << "Target is at index: " << LinearSearch(array00, 20, 3);
+	cout << "Target is: 3 |" << "| Target is at index: " << LinearSearch(array00, 20, 3) << endl;
+	assert(LinearSearch(array00,20,3) == 2);
+
+	cout << "Target is : 7 |" << "| Target is at index : " << LinearSearch(array00, 20, 7) << endl;
+	assert(LinearSearch(array00, 20, 7) == 11);
+
+	cout << "Target is : 67 |" << "|Target is at index: " << LinearSearch(array00, 20, 67) << endl;
+	assert(LinearSearch(array00, 20, 67) == 0);
+
+	cout << "Target is : 88 |" << "|Target is at index: " << LinearSearch(array00, 20, 88) << endl;
+	assert(LinearSearch(array00, 20, 88) == -1);
 }
 
 int LinearSearch(int array[], int size, int target)
