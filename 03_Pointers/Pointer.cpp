3652cb173406029d46721450dd4d1d7278b7068c
Te Poro <teporofletcher47@gmail.com>
2026-02-21 12:20:00 +1100

    Staging??

--- 03_Pointers/Pointer.cpp
+++ 03_Pointers/Pointer.cpp
@@ -7,11 +7,13 @@ void PointerSwap(int* a, int* b);
 
 int main()
 {
+	// the variables
 	int a = 8;
 	int b = 69;
 
 	PointerSwap(&a, &b);
 
+	// testing
 	assert(a == 69);
 	assert(b == 8);
 }
@@ -19,7 +21,7 @@ int main()
 // use temp var to help swap values
 void PointerSwap(int* a, int* b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	int temp = *a;// hold a to swap to other variable
+	*a = *b;// puts b in a
+	*b = temp;// puts a in b
 }
